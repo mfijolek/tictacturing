34f5362b8c34549bc18fe8bb18fa2a27fc89f439
Michał Fijołek <mfijolek@connectmedica.local>
2017-06-28 09:25:40 +0200

    nev drawer added

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,21 +1,15 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import RaisedButton from 'material-ui/RaisedButton'
-injectTapEventPlugin();
+import NavDrawer from '../components/NavDrawer.js'
+injectTapEventPlugin()
+
 class Template extends Component {
 	render() {
 		return (
 			<MuiThemeProvider>
 				<div>
-					<header>
-						<h1>TicTacTuring</h1>
-						<RaisedButton
-							label={'Test Button'}
-							primary={true}
-							onTouchTap={()=>{console.log("hello, I work")}}
-						/>
-					</header>
+					<NavDrawer/>
 					<main>
 						{this.props.children}
 					</main>
